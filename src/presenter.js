f2726d22eb36126648c42ca4cb9aacfe2a3f3fe9
“Cristian <cristiancanapi730@gmail.com>
2023-09-03 22:24:52 -0400

    Ingresar puntos maximos de la superficie y mostrarlos en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,20 @@
 import validarPI from "./validarPI";
 const pos_inicial = document.querySelector("#posicion-inicial");
-
+const puntos_max = document.querySelector("#puntos-maximos")
 const form = document.querySelector("#autitos-form");
-const div = document.querySelector("#resultado-div");
-
+const div = document.querySelector("#posicion-div");
+const div2 = document.querySelector("#puntosMAX")
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
 
   const posicionInicial = pos_inicial.value;
+  const punMaximos = puntos_max.value;
   const validar = validarPI(posicionInicial);
-  const textoValidar = " ";
   if(validar == true)
   {
     div.innerHTML = "<p> Posicion inicial: " + posicionInicial + "</p>";
+    div2.innerHTML = "<p> Puntos maximos: " + punMaximos + "</p>"
   }
   else{
     div.innerHTML = "<p> Formato de Posicion inicial incorrecta porfavor vuelve a ingresarla " + " "+ "</p>";
