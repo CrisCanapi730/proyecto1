ec2ae6598c2ee1d90001bf6e85298311636d7116
“Cristian <cristiancanapi730@gmail.com>
2023-09-03 23:20:24 -0400

    Escribir comandos y mostrarlos en pantalla

--- src/presenter.js
+++ src/presenter.js
@@ -2,15 +2,19 @@ import validarPI from "./validarPI";
 import validarPuntosMax from "./validarPuntos";
 const pos_inicial = document.querySelector("#posicion-inicial");
 const puntos_max = document.querySelector("#puntos-maximos")
+const comds = document.querySelector("#comandos")
 const form = document.querySelector("#autitos-form");
 const div = document.querySelector("#posicion-div");
-const div2 = document.querySelector("#puntosMAX")
+const div2 = document.querySelector("#puntosMAX");
+const div3 = document.querySelector("#comandos-div");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
 
   const posicionInicial = pos_inicial.value;
   const punMaximos = puntos_max.value;
+  const comandos = comds.value;
   const validar = validarPI(posicionInicial);
   const validarPuntos = validarPuntosMax(punMaximos);
 
@@ -24,6 +28,7 @@ form.addEventListener("submit", (event) => {
     else{
       div2.innerHTML = "<p> Formato de Puntos maximos de la superficia incorrecta porfavor vuelve a ingresarla " + " "+ "</p>";
     }
+    div3.innerHTML = "<p> Comandos " + comandos + "</p>";
     
   }
   else{
@@ -35,7 +40,8 @@ form.addEventListener("submit", (event) => {
     else{
       div2.innerHTML = "<p> Formato de Puntos maximos de la superficia incorrecta porfavor vuelve a ingresarla " + " "+ "</p>";
     }
-    
+    div3.innerHTML = "<p> Comandos: " + comandos + "</p>";
+
   }
 
   
